3acb7106eb0007649d1d5d2bd52c2f135763cc00
Akshita Patel <akshitapatel02@gmail.com>
2021-04-17 16:46:00 +0530

    Updating server.js for app.listen

--- server.js
+++ server.js
@@ -36,4 +36,6 @@ app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
